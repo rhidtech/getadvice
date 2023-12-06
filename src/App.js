78bf2a92b529c000bdae585210b301ec4ac26d18
rhidtech <abdulsalamridwan16@gmail.com>
2023-12-06 18:01:13 +0100

    generate random valid advice

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((d) => d + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        you have generated <strong>{count}</strong> pieces of advice
+      </p>
+    </div>
+  );
+}
